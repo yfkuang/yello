a4cbb07b0ba139ae30331cb4b0f6fcf41a384feb
yfkuang <contact@yfkuang.com>
2018-05-15 16:30:54 -0400

    Voice Message

--- resources/views/call.blade.php
+++ resources/views/call.blade.php
@@ -1,28 +1,31 @@
 <?php
+	//use Twilio\Rest\Client;
+	
+	//Your Account SID and Auth Token from twilio.com/console
+	/*$account_sid = 'AC1a67400c4075f16f092296d0db4f8220';
+	$auth_token = '3a4e85551888226c80235d42a6f96614';
+	
+	$twilio_number = "+18193030170";
+	
+	$twilio = new Client($account_sid, $auth_token);
+	$call = $twilio->calls->create("+16137971900", $twilio_number,
+                           array(
+								'method' => "GET",
+								'statusCallback' => "https://www.myapp.com/events",
+								'statusCallbackEvent' => "initiated",
+								'statusCallbackMethod' => "POST",
+								'url' => "http://phplaravel-73309-509403.cloudwaysapps.com/preface.xml"
+							)
+                  );*/
 	use Twilio\Twiml;
 
 	$response = new Twiml();
-	$response->say('Hello World');
-	$response->play('https://api.twilio.com/Cowbell.mp3');
-
+	$response->dial('+16137971900');
+	
 	echo $response;
-
-	/*// Your Account SID and Auth Token from twilio.com/console
-	$account_sid = 'AC1a67400c4075f16f092296d0db4f8220';
-	$auth_token = '3a4e85551888226c80235d42a6f96614';
+	//$response->dial('+16137971900');
+	
 	// In production, these should be environment variables. E.g.:
 	//$auth_token = $_ENV["3a4e85551888226c80235d42a6f96614"];
 
-	// A Twilio number you own with SMS capabilities
-	$twilio_number = "+18193030170";
-
-	$client = new Client($account_sid, $auth_token);
-	$client->messages->create(
-		// Where to send a text message (your cell phone?)
-		'+16137971900',
-		array(
-			'from' => $twilio_number,
-			'body' => 'more shit!'
-		)
-	);*/
 ?>
---
+++ resources/views/preface.blade.php
@@ -0,0 +1,5 @@
+<script id="myxml" type="text/xmldata">
+<Response>
+	<Say voice="alice">This voice service is forwarded by LeadLabs Marketing.</Say>
+</Response>
+</script>
